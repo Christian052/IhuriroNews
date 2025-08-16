3f1ef5591786773fec7e7f5d0adfb137d652520f
Christian052 <doctorshavu@gmail.com>
2025-08-16 11:47:27 +0200

    second push

--- Backend/server.js
+++ Backend/server.js
@@ -46,7 +46,7 @@ app.use(
 
 // Connect to MongoDB
 mongoose
-  .connect(MONGO_URI)
+  .connect(process.env.MONGO_URI || MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
